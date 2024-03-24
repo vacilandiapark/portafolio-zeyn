99129b3736880677ef43a536027fd25a38fb5bbb
Vacilandia Park <michaeld@vacilandia.com.pe>
2024-03-24 17:13:18 -0500

    update

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+
+window.addEventListener('scroll', function () {
+    var subnavbar = document.getElementById('subnavbar');
+    if (window.scrollY > 0) {
+        subnavbar.classList.add('white-bg');
+    } else {
+        subnavbar.classList.remove('white-bg');
+    }
+}); 
+
+*/
